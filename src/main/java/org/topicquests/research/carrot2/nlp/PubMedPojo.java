a94874204330cc04d433c4b33351ae9b84426190
Jack Park <jackpark@gmail.com>
2024-03-24 14:01:56 -0700

    Started ES support

---
+++ src/main/java/org/topicquests/research/carrot2/nlp/PubMedPojo.java
@@ -0,0 +1,165 @@
+/**
+ * 
+ */
+package org.topicquests.research.carrot2.nlp;
+
+import java.util.Map;
+
+import net.minidev.json.JSONObject;
+
+/**
+ * @author jackpark
+ * Based on Elasticsearch mappings found here</br>
+ * https://raw.githubusercontent.com/iross/km_indexer/master/index_pubmed.py
+ */
+public class PubMedPojo extends JSONObject {
+
+	/**
+	 * 
+	 */
+	public PubMedPojo() {
+	}
+
+	/**
+	 * @param map
+	 */
+	public PubMedPojo(Map<String, ?> map) {
+		super(map);
+	}
+
+	public void setAbstract(String txt) {
+		
+	}
+	
+	public String getAbstract() {
+		return null;
+	}
+	
+	public void setAbstractLongForm(String txt) {
+		
+	}
+	
+	public String getAbstractLongForm() {
+		return null;
+	}
+
+	public void setChemicalName(String txt) {
+		
+	}
+	
+	public String getChemicalName() {
+		return null;
+	}
+
+	public void setChemicalUi(String txt) {
+		
+	}
+	
+	public String getChemicalUi() {
+		return null;
+	}
+
+	public void setContentURL(String txt) {
+		
+	}
+	
+	public String getContentURL() {
+		return null;
+	}
+	
+	public void setCoverDate(String txt) {
+		
+	}
+	
+	public String getCoverDate() {
+		return null;
+	}
+
+	public void setCover_Date(String txt) {
+		
+	}
+	
+	public String getCover_Date() {
+		return null;
+	}
+
+	public void setIssue(String txt) {
+		
+	}
+	
+	public String getIssue() {
+		return null;
+	}
+
+	public void setMeshDescriptorName(String txt) {
+		
+	}
+	
+	public String getMeshDescriptorName() {
+		return null;
+	}
+
+	public void setMeshDescriptorUi(String txt) {
+		
+	}
+	
+	public String getMeshDescriptorUi() {
+		return null;
+	}
+
+	public void setMeshQualifierName(String txt) {
+		
+	}
+	
+	public String getMeshQualifierName() {
+		return null;
+	}
+	public void setMeshQualifierUi(String txt) {
+		
+	}
+	
+	public String getMeshQualifierUi() {
+		return null;
+	}
+
+	public void setPMID(String txt) {
+		
+	}
+	
+	public String getPMID() {
+		return null;
+	}
+	public void setPublicationDate(String txt) {
+		
+	}
+	
+	public String getPublicationDate() {
+		return null;
+	}
+	
+	public void setTime(String txt) {
+		
+	}
+	
+	public String getTime() {
+		return null;
+	}
+
+	public void setTitle(String txt) {
+		
+	}
+	
+	public String getTitlee() {
+		return null;
+	}
+
+	public void setVol(String txt) {
+		
+	}
+	
+	public String getVol() {
+		return null;
+	}
+
+
+}
