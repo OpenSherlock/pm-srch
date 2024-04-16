66950eb67c2d16669be88d628d63e28046954154
Jack Park <jackpark@gmail.com>
2024-04-16 11:46:25 -0700

    added doctypes

---
+++ src/main/java/org/topicquests/os/asr/api/IDocumentType.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.topicquests.os.asr.api;
+
+/**
+ * @author jackpark
+ */
+public interface IDocumentType {
+	public static final String
+		PUBMD		= "pubmed",
+		PMC			= "pmc",
+		UMLS		= "umls",
+		DBPEDIA		= "dbped",
+		WIKIDATA	= "wiki",
+		XRIV		= "xriv",
+		REPORT		= "rept",
+		BLOG		= "blog";
+}
