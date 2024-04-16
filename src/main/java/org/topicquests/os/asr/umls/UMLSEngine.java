b1b88f1c7ac406647a106261d4dfd8efe6a65359
Jack Park <jackpark@gmail.com>
2024-04-16 14:04:57 -0700

    WIP

--- src/main/java/org/topicquests/os/asr/umls/UMLSEngine.java
+++ src/main/java/org/topicquests/os/asr/umls/UMLSEngine.java
@@ -3,17 +3,21 @@
  */
 package org.topicquests.os.asr.umls;
 
+import org.topicquests.research.carrot2.Environment;
+
 /**
- * 
+ * @author jackpark
+ * We harvest over an eventstream from a UMLS file reader
  */
 public class UMLSEngine {
-
-	private final String REDIS_TOPIC = "dbpJson";
+	private Environment environment;
+	private boolean isRunning = true;
 	/**
 	 * 
 	 */
-	public UMLSEngine() {
-		// TODO Auto-generated constructor stub
+	public UMLSEngine(Environment env) {
+		environment = env;
+
 	}
 
 }
