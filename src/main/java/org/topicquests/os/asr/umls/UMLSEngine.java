a91778369200f6947fe09f4b46b1b114860edef3
Jack Park <jackpark@gmail.com>
2024-04-16 13:56:34 -0700

    Starting umls

---
+++ src/main/java/org/topicquests/os/asr/umls/UMLSEngine.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.topicquests.os.asr.umls;
+
+/**
+ * 
+ */
+public class UMLSEngine {
+
+	private final String REDIS_TOPIC = "dbpJson";
+	/**
+	 * 
+	 */
+	public UMLSEngine() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
