3daf0e3adfb7c460c5a24937b40cae4654e24cd4
Jack Park <jackpark@gmail.com>
2024-04-16 19:43:45 -0700

    new abstract class

---
+++ src/main/java/org/topicquests/os/asr/enigines/AbstractRedisEngine.java
@@ -0,0 +1,80 @@
+/**
+ * 
+ */
+package org.topicquests.os.asr.enigines;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.topicquests.os.asr.Environment;
+import org.topicquests.os.asr.RedisClient;
+
+/**
+ * 
+ */
+public abstract class AbstractRedisEngine {
+	protected Environment environment;
+	private boolean isRunning = true;
+	protected boolean hasBeenRunning = true;
+	public RedisClient redis;
+	private List<String> docs;
+	private Worker worker;
+	/**
+	 * 
+	 */
+	public AbstractRedisEngine(Environment env) {
+		environment = env;
+		redis = environment.getRedis();
+		docs = new ArrayList<String>();
+		isRunning = true;
+		worker = new Worker();
+		worker.start();
+	}
+	
+	
+	abstract void processDoc(String json);
+	
+	public void addDoc(String xml) {
+		environment.logDebug("PT.add");
+		synchronized(docs) {
+			docs.add(xml);
+			docs.notify();
+		}
+	}
+
+	
+	class Worker extends Thread {
+		public void run() {
+			environment.logDebug("PubMedEngine.starting");
+			String doc = null;
+			while (isRunning) {
+				synchronized(docs) {
+					if (docs.isEmpty()) {
+						if (hasBeenRunning) {
+							environment.queueEmpty();
+							hasBeenRunning  = false;
+						}
+						try {
+							docs.wait();
+						} catch (Exception e) {}
+						
+					} else {
+						doc = docs.remove(0);
+					}
+				}
+				if (isRunning && doc != null) {
+					processDoc(doc);
+					doc = null;
+				}
+			}
+		}
+	}
+	
+	public void shutDown() {
+		synchronized(docs) {
+			isRunning = false;
+			docs.notify();
+		}
+	}
+
+}
